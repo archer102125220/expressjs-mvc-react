923a87c1aa4ecb1c8c8df4a23a10084c91a42ad2
陳柏杰 <archer10212522.2015@gmail.com>
2021-05-31 16:39:37 +0800

    SPA router test

--- script/config/router/reactRouter.js
+++ script/config/router/reactRouter.js
@@ -1,7 +1,9 @@
 import IndexPage from '@views';
+import TestPage from '@views/testPage';
 
 export const routeComponent = [
     { key: 'root', path: '/', exact: true, component: IndexPage, pageName: 'index' },
+    { key: 'test', path: '/testPage', component: TestPage, pageName: 'testPage' },
 ];
 export const redirectComponent = [
     { key: 'root', exact: true, to: '/', From: '/index' },
--- script/views/testPage.js
+++ script/views/testPage.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Logo from '@server/public/assets/logo.svg';
 import '@server/public/stylesheets/App.css';
+import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import Button from '@utils/components/Button';
+import Socket from '@socket/socketIoClient';
 // https://stackoverflow.com/questions/57012780/adding-css-to-react-ssr-components
 
+
+//https://www.sipios.com/blog-tech/how-to-use-styled-components-with-material-ui-in-a-react-app
+const styles = {
+  normal: {
+    fontFamily: 'Georgia, sans-serif',
+    marginTop: '3em',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: '2.5rem',
+    fontWeight: 'normal',
+    letterSpacing: '-1px',
+  },
+  list: {
+    fontSize: '1.2em',
+    marginTop: '1.8em',
+    listStyle: 'none',
+    lineHeight: '1.5em',
+    '& code': {
+      background: '#f7f7f7',
+    }
+  },
+};
 
 const mapStateToProps = (state) => ({
   users: state.userList?.userList || [],
@@ -15,7 +40,7 @@ const mapDispatchToProps = (dispatch) => ({
   GET_UserList: (payload, callback, loading) => dispatch({ type: 'userList/GET_UserList', payload, callback, loading }),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(
   class Index extends Component {
     constructor(props) {
       super(props);
@@ -31,6 +56,8 @@ export default connect(mapStateToProps, mapDispatchToProps)(
     }
 
     render() {
+      const { users } = this.props;
+      const buttons = users.map(element => ({ element: element.account, event: () => Socket.clickEventSend({ id: 10 }) }));
       return (
         <div className="App">
           <header className="App-header">
@@ -44,8 +71,8 @@ export default connect(mapStateToProps, mapDispatchToProps)(
               rel="noopener noreferrer"
             >
               Learn React
-          </a>
-          <Link to="/testPage" className="navbar-brand">SPA換頁測試</Link>
+            </a>
+            <Button buttons={buttons} />
           </header>
         </div>);
     }
@@ -54,10 +81,11 @@ export default connect(mapStateToProps, mapDispatchToProps)(
       children: PropTypes.any,
       history: PropTypes.any,
       title: PropTypes.string,
+      users: PropTypes.array,
     };
 
     static defaultProps = {
       title: '12345'
     }
   }
-);
+));
