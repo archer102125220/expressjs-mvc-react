c4462e46e194b96e7b45ec90089655a9daf28ee4
Archer <archer102125220.2015@gmail.com>
2021-07-23 10:58:44 +0800

    resolution test

--- script/views/video/Player.js
+++ script/views/video/Player.js
@@ -27,10 +27,9 @@ export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(
     render() {
       return (
         <div>
-          <VideoPlayer src='/video/video-Tate no Yuusha no Nariagari 16 [BD 1920x1080 HEVC x265 10bit ASSx2]-Marnie-1625073213454.mp4' />
-          <VideoPlayer src='/video/video-[DMG] 甘城ブリリアントパーク 第01話「お客が来ない!」 [BDRip][AVC_AAC][720P][CHT](1C38355F)-1623743666230.mp4' />
-          <VideoPlayer src='/assets/video/test_mp4_3.mp4' />
-          <VideoPlayer src='/video/video-[WMSUB][Digimon Adventure][LAST EVOLUTION-Kizuna][BDRip][Childhood translation][BIG5][1080P]-Marnie-1625107838014.mp4' />
+        <VideoPlayer src='/video/video-[Kamigami&VCB-Studio] Yahari Ore no Seishun Lovecome wa Machigatte Iru. Zoku [OVA][Ma10p_1080p][x265_flac]-parker-1626973814647.mp4' />
+          <VideoPlayer src='/video/video-[WMSUB][Digimon Adventure][LAST EVOLUTION-Kizuna][BDRip][Childhood translation][BIG5][1080P]-parker-1626973814647.mp4' />
+          <VideoPlayer src='/video/video-Yuru Camp 01 [BD 1920x1080 HEVC-10bit AAC ASSx2]-parker-1626973814647.mp4' />
         </div>
       );
     }
