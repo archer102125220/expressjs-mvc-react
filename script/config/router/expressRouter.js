786856c9259337c478625710aff3280d56bfbff4
陳柏杰 <archer10212522.2015@gmail.com>
2021-07-24 22:51:41 +0800

    api document update

--- script/config/router/expressRouter.js
+++ script/config/router/expressRouter.js
@@ -25,50 +25,98 @@ class ExpressRouterConstructor {
 
   createRoutesWeb = () => {
     const routesWeb = Express.Router();
-    routesWeb.get('/', indexController.homePage);
-    routesWeb.get('/login', indexController.login);
-    routesWeb.get('/video/player', indexController.player);
+    routesWeb.get('/',
+      /*
+        #swagger.tags = ['web']
+      */
+      indexController.homePage);
+    routesWeb.get('/login',
+      /*
+        #swagger.tags = ['web']
+      */
+      indexController.login);
+    routesWeb.get('/video/player',
+      /*
+        #swagger.tags = ['web']
+      */
+      indexController.player);
     this.routesWeb = routesWeb;
   }
   createRoutesApi = () => {
     const routesApi = Express.Router();
-    routesApi.get('/', indexController.homePageAPI);
-    routesApi.get('/users', usersController.usersList);
-    routesApi.post('/users/registered', uploader.avater(), usersController.createUser);
-    routesApi.post('/users/img_upload_test', uploader.singleImg(), usersController.imgUploadTest);
+    routesApi.get('/',
+      /*
+        #swagger.tags = ['api']
+        #swagger.path = '/api'
+      */
+      indexController.homePageAPI);
+    routesApi.get('/users',
+      /*
+        #swagger.tags = ['api', 'users']
+        #swagger.path = '/api/users'
+      */
+      usersController.usersList);
+    routesApi.post('/users/registered',
+      /*
+        #swagger.tags = ['api', 'users']
+        #swagger.path = '/api/users/registered'
+      */
+      uploader.avater(), usersController.createUser);
+    routesApi.post('/users/img_upload_test',
+      /*
+        #swagger.tags = ['api', 'users']
+        #swagger.path = '/api/users/img_upload_test'
+      */
+      uploader.singleImg(), usersController.imgUploadTest);
     routesApi.post('/users/video_upload_test',
       /*
-          #swagger.path = '/api/users/video_upload_test'
-          #swagger.method = 'post'
-          #swagger.description = 'Upload video and subtitle.'
-          #swagger.parameters['video'] = {
-              in: 'body',
-              type:'object',
-              description: 'Upload video.' }
-          #swagger.responses[200] = {
-              description: 'Upload complete and convert start.',
-              schema: {}
-          } 
+        #swagger.tags = ['api', 'users']
+        #swagger.path = '/api/users/video_upload_test'
+        #swagger.method = 'post'
+        #swagger.description = 'Upload video and subtitle.'
+        #swagger.consumes = 'multipart/form-data'
+        #swagger.parameters['videoOptionList'] = {
+          in: 'formData',
+          type: 'txt',
+          description: 'Upload video config.',
+          example: '[{ originName: videoName, timestamp: Date.now(), newName: \'newName\', \'ssa-file\': subtitleName, \'ssa-burn\': 1 }]'
+        }
+        #swagger.parameters['video'] = {
+          in: 'formData',
+          type: 'array',
+          items: { type: 'file' },
+          description: 'Upload video and subtitle.'
+        }
+        #swagger.responses[200] = {
+          description: 'Upload complete and convert start.',
+          schema: '檔案上傳成功！開始轉檔...',
+        }
       */
       uploader.arrayVideo(), usersController.videoUpload);
 
     // this.post('/users/video_upload_test', uploader.singleVideo(), usersController.videoUpload);
     routesApi.get('/users/account/:name',
       /*
-          #swagger.path = '/api/users/account/:name'
-          #swagger.method = 'get'
-          #swagger.description = 'Query a user.'
-          #swagger.parameters['name'] = {
-              in: 'path',
-              description: 'User name.' }
-          #swagger.responses[200] = {
-              description: 'User info.',
-              schema: {}
-          } 
+        #swagger.tags = ['api', 'users']
+        #swagger.path = '/api/users/account/:name'
+        #swagger.method = 'get'
+        #swagger.description = 'Query a user.'
+        #swagger.parameters['name'] = {
+          in: 'path',
+          description: 'User name.' }
+        #swagger.responses[200] = {
+          description: 'User info.',
+          schema: {}
+        }
       */
       usersController.findUser);
 
-    routesApi.post('/users/login', usersController.loginUser);
+    routesApi.post('/users/login',
+      /*
+        #swagger.tags = ['api', 'users']
+        #swagger.path = '/api/users/login'
+      */
+      usersController.loginUser);
     this.routesApi = routesApi;
   }
 
